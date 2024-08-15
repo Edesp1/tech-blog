ad5d8c48df3b37d89e3fa17edc92c84f268bc477
unknown <edesp.lopez17@gmail.com>
2024-08-15 13:55:07 -0700

    please please

--- config/connections.js
+++ config/connections.js
@@ -1,22 +1,12 @@
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-// create connection to our db
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
-    logging: (msg) => {
-      if (process.env.NODE_ENV === 'development') {
-        console.log(msg);
-      }
-    },
-  }
-);
-
-module.exports = sequelize;
+// Create connection to our database using DATABASE_URL from Heroku
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  logging: (msg) => {
+    if (process.env.NODE_ENV === 'development') {
+      console.log(msg);
+    }
+  },
+});
