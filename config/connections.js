8aaa4da9014e5fa836ab9b75b1af48dff241615b
unknown <edesp.lopez17@gmail.com>
2024-08-15 14:06:04 -0700

    dsome

--- config/connections.js
+++ config/connections.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Create connection to our database using DATABASE_URL from Heroku
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.JAWSDB_URL, {
   dialect: 'mysql',
   logging: (msg) => {
     if (process.env.NODE_ENV === 'development') {
